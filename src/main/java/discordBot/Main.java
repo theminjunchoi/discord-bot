f20bdda40d9c3ff14359b8ee0ba25fbee11fc84d
MinJun Choi <mj043000@naver.com>
2025-05-31 01:00:23 +0900

    fix: replace environment variable with hardcoded token debug

--- src/main/java/discordBot/Main.java
+++ src/main/java/discordBot/Main.java
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.JDABuilder;
 
 public class Main {
     public static void main(String[] args) throws LoginException {
-        String token = System.getenv("DISCORD_BOT_TOKEN");
+        String token = System.getenv("MTM3ODAyNjkzNjUwODQ4NTczMw.GLPfVq.fk-l8QcT2e2DGk9N5St6S_NhLnbb_ZOKLrFKxM");
 
         JDABuilder.createDefault(token)
                 .addEventListeners(new MessageListener())
