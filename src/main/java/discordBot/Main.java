be3216cead7207beec761aea41f687526708051a
MinJun Choi <mj043000@naver.com>
2025-05-31 01:15:40 +0900

    feat: 디스코드 봇에 GatewayIntent 설정 추가

--- src/main/java/discordBot/Main.java
+++ src/main/java/discordBot/Main.java
@@ -1,13 +1,16 @@
 package discordBot;
 
+import java.util.EnumSet;
 import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Main {
     public static void main(String[] args) throws LoginException {
         String token = System.getenv("DISCORD_BOT_TOKEN");
 
         JDABuilder.createDefault(token)
+                .enableIntents(EnumSet.of(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES))
                 .addEventListeners(new MessageListener())
                 .build();
     }
