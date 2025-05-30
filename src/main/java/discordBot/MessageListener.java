2ae4fba966b9c497a3ac61e1c05301bbaca861d7
MinJun Choi <mj043000@naver.com>
2025-05-31 01:45:12 +0900

    feat: 디스코드 봇에 GatewayIntent 설정 추가

--- src/main/java/discordBot/MessageListener.java
+++ src/main/java/discordBot/MessageListener.java
@@ -9,12 +9,16 @@ public class MessageListener extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event) {
         String msg = event.getMessage().getContentRaw();
 
-        if (event.getAuthor().isBot()) return;
+        if (event.getAuthor().isBot()) {
+            return;
+        }
 
         if (msg.startsWith("와")) {
             event.getChannel().sendMessage("샌즈! 아시는구나!\n이거 겁나 어렵습니다.").queue();
         } else if (msg.contains("자라")) {
-            event.getChannel().sendMessage("전기세 아깝다ㅡㅡ;;").queue();
+            event.getChannel().sendMessage("잠은 죽어서 잔다").queue();
+        } else if (msg.contains("참깨스틱") || msg.contains("push")) {
+            event.getChannel().sendMessage("갖고와~").queue();
         }
     }
 }
