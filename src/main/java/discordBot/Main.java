fd6119276fddcb691cebc2325b2c3c2c06538827
MinJun Choi <mj043000@naver.com>
2025-05-31 00:55:36 +0900

    refactor: 환경 변수 로드 방식을 System.getenv()로 수정

--- src/main/java/discordBot/Main.java
+++ src/main/java/discordBot/Main.java
@@ -1,13 +1,11 @@
 package discordBot;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.JDABuilder;
 
 public class Main {
     public static void main(String[] args) throws LoginException {
-        Dotenv dotenv = Dotenv.load(); // .env 자동 읽음
-        String token = dotenv.get("DISCORD_BOT_TOKEN");
+        String token = System.getenv("DISCORD_BOT_TOKEN");
 
         JDABuilder.createDefault(token)
                 .addEventListeners(new MessageListener())
