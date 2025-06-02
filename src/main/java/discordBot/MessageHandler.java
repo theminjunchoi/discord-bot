1ee49c62d3dd381c0b3d6fecc22733a87506c152
MinJun Choi <mj043000@naver.com>
2025-06-02 13:12:45 +0900

    feat: 멤버별 메시지 응답 기능 추가 및 메시지 처리 로직 확장

--- src/main/java/discordBot/MessageHandler.java
+++ src/main/java/discordBot/MessageHandler.java
@@ -3,18 +3,52 @@ package discordBot;
 public class MessageHandler {
 
     public String handle(String msg) {
+
+        // MEMBER
+        if (msg.contains("에드")) {
+            return RandomPicker.chooseFrom(Answer.ED.getMessages());
+        }
+        if (msg.contains("엠제이")) {
+            return RandomPicker.chooseFrom(Answer.MJ.getMessages());
+        }
+        if (msg.contains("칼리")) {
+            return RandomPicker.chooseFrom(Answer.KALI.getMessages());
+        }
+        if (msg.contains("제프")) {
+            return RandomPicker.chooseFrom(Answer.JEFF.getMessages());
+        }
+        if (msg.contains("띠용")) {
+            return RandomPicker.chooseFrom(Answer.DDIYONG.getMessages());
+        }
+        if (msg.contains("폰트")) {
+            return RandomPicker.chooseFrom(Answer.FONT.getMessages());
+        }
+        if (msg.contains("저스틴")) {
+            return RandomPicker.chooseFrom(Answer.JUSTIN.getMessages());
+        }
+        if (msg.contains("벡터")) {
+            return RandomPicker.chooseFrom(Answer.VECTOR.getMessages());
+        }
+        if (msg.contains("링크")) {
+            return RandomPicker.chooseFrom(Answer.LINK.getMessages());
+        }
+        if (msg.contains("강산")) {
+            return RandomPicker.chooseFrom(Answer.GANGSAN.getMessages());
+        }
+        if (msg.contains("율무")) {
+            return RandomPicker.chooseFrom(Answer.YULMOO.getMessages());
+        }
+
+        // ETC
         if (msg.startsWith("와")) {
             return RandomPicker.chooseFrom(Answer.SANS.getMessages());
         }
-
         if (msg.contains("자라")) {
             return RandomPicker.chooseFrom(Answer.JAM.getMessages());
         }
-
         if (msg.contains("참깨스틱") || msg.contains("push")) {
             return RandomPicker.chooseFrom(Answer.ED.getMessages());
         }
-
         return "";
     }
 }
